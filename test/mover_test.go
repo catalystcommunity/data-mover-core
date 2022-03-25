f554b2aa7814da33c06abb6c09f1ef4e5d5558a1
zack stevens <zack.st7@gmail.com>
2022-03-25 10:54:29 -0600

    norelease: Refactored tests into testify test suite, and re-ordered things, added comments, for clarity on what's a test, what's an implementation, etc.

--- test/mover_test.go
+++ test/mover_test.go
@@ -6,60 +6,44 @@ import (
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/catalystsquad/data-mover-core/pkg"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"sort"
 	"sync"
 	"sync/atomic"
 	"testing"
 )
 
-var numIterations = gofakeit.Number(10000, 20000)
-var executedIterations = 0
-var sourceData []map[string]interface{}
-var destData []map[string]interface{}
 var lock = new(sync.Mutex)
-var expectedRecords = int64(0)
+var numIterations, executedIterations int
+var sourceData, destData []map[string]interface{}
+var expectedRecords int64
 
-type TestSource struct{}
-
-func (t TestSource) Initialize() error {
-	return nil
+// test suite definition and setup functions
+type MoverSuite struct {
+	suite.Suite
 }
 
-func (t TestSource) GetData() ([]map[string]interface{}, error) {
-	data := []map[string]interface{}{}
-	if executedIterations < numIterations {
-		executedIterations++
-		numRecords := gofakeit.Number(1, 3)
-		for i := 0; i < numRecords; i++ {
-			numKeys := gofakeit.Number(1, 3)
-			record := map[string]interface{}{}
-			for i := 0; i < numKeys; i++ {
-				record[gofakeit.Name()] = gofakeit.HackerPhrase()
-			}
-			data = append(data, record)
-			atomic.AddInt64(&expectedRecords, 1)
-		}
-	}
-	// append generated data to source data in a thread safe manner
-	appendSourceData(data)
-	return data, nil
+// empty for now, implement if needed
+func (s *MoverSuite) SetupSuite() {}
+
+// empty for now, implement if needed
+func (s *MoverSuite) TearDownSuite() {}
+
+func (s *MoverSuite) SetupTest() {
+	// initialize vars before each test
+	numIterations = gofakeit.Number(10000, 20000)
+	expectedRecords, executedIterations = 0, 0
+	sourceData, destData = []map[string]interface{}{}, []map[string]interface{}{}
 }
 
-type TestDestination struct{}
-
-func (t TestDestination) Initialize() error {
-	return nil
+// tests
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(MoverSuite))
 }
 
-func (t TestDestination) Persist(data []map[string]interface{}) error {
-	// append data to dest data in a thread safe manner
-	appendDestData(data)
-	return nil
-}
-
-func TestConcurrentMove(t *testing.T) {
-	source := TestSource{}
-	dest := TestDestination{}
+func (s *MoverSuite) TestConcurrentMove() {
+	source := TestConcurrentMoveSource{}
+	dest := TestConcurrentMoveDestination{}
 	errorHandler := func(err error) bool {
 		fmt.Println(fmt.Sprintf("encountered error: %v", err))
 		return true
@@ -72,21 +56,22 @@ func TestConcurrentMove(t *testing.T) {
 		errorHandler,
 		errorHandler,
 	)
-	assert.NoError(t, err)
-	assert.NotNil(t, mover)
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), mover)
 	stats, err := mover.Move()
-	assert.NoError(t, err)
-	assert.Equal(t, expectedRecords, int64(len(sourceData)))
-	assert.Equal(t, expectedRecords, int64(len(destData)))
-	assert.Equal(t, getDataAsSortedString(t, sourceData), getDataAsSortedString(t, destData))
-	assert.Equal(t, uint64(len(sourceData)), stats.SourceCount)
-	assert.Equal(t, uint64(len(destData)), stats.DestinationCount)
-	assert.Len(t, stats.SourceErrors, 0)
-	assert.Len(t, stats.DestinationErrors, 0)
-	assert.Greater(t, stats.Duration.Microseconds(), int64(0)) // this is very fast because it's a unit test, milliseconds comes out as 0, so using microseconds
-	assert.Greater(t, stats.RecordsPerSecond, float64(0))
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), expectedRecords, int64(len(sourceData)))
+	assert.Equal(s.T(), expectedRecords, int64(len(destData)))
+	assert.Equal(s.T(), getDataAsSortedString(s.T(), sourceData), getDataAsSortedString(s.T(), destData))
+	assert.Equal(s.T(), uint64(len(sourceData)), stats.SourceCount)
+	assert.Equal(s.T(), uint64(len(destData)), stats.DestinationCount)
+	assert.Len(s.T(), stats.SourceErrors, 0)
+	assert.Len(s.T(), stats.DestinationErrors, 0)
+	assert.Greater(s.T(), stats.Duration.Microseconds(), int64(0)) // this is very fast because it's a unit tess.T(), milliseconds comes out as 0, so using microseconds
+	assert.Greater(s.T(), stats.RecordsPerSecond, float64(0))
 }
 
+// helper functions
 func getDataAsSortedString(t *testing.T, data []map[string]interface{}) string {
 	bytes, err := json.Marshal(data)
 	assert.NoError(t, err)
@@ -108,4 +93,43 @@ func appendDestData(data []map[string]interface{}) {
 	lock.Lock()
 	defer lock.Unlock()
 	destData = append(destData, data...)
+}
+
+// source and destination implementations below
+type TestConcurrentMoveSource struct{}
+
+func (t TestConcurrentMoveSource) Initialize() error {
+	return nil
+}
+
+func (t TestConcurrentMoveSource) GetData() ([]map[string]interface{}, error) {
+	data := []map[string]interface{}{}
+	if executedIterations < numIterations {
+		executedIterations++
+		numRecords := gofakeit.Number(1, 3)
+		for i := 0; i < numRecords; i++ {
+			numKeys := gofakeit.Number(1, 3)
+			record := map[string]interface{}{}
+			for i := 0; i < numKeys; i++ {
+				record[gofakeit.Name()] = gofakeit.HackerPhrase()
+			}
+			data = append(data, record)
+			atomic.AddInt64(&expectedRecords, 1)
+		}
+	}
+	// append generated data to source data in a thread safe manner
+	appendSourceData(data)
+	return data, nil
+}
+
+type TestConcurrentMoveDestination struct{}
+
+func (t TestConcurrentMoveDestination) Initialize() error {
+	return nil
+}
+
+func (t TestConcurrentMoveDestination) Persist(data []map[string]interface{}) error {
+	// append data to dest data in a thread safe manner
+	appendDestData(data)
+	return nil
 }
